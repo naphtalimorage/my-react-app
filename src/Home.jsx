0410bfc8e8bcd012a5bbe6abe37adfc221789018
Naphtali Morage <naphtalimorage56@gmail.com>
2024-05-28 22:49:12 +0300

    make neat and interactive

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+
+const Home = () => {
+    
+  return (
+    <div className="App">
+      <h1 className="first-header">Laikipia Concert,</h1>
+      <h1 className="second-header">Welcome to the backstage</h1>
+      <Link to="/register">
+        <button className="regester-btn">Register</button>
+      </Link>
+    </div>
+  );
+};
+
+
+export default Home;
