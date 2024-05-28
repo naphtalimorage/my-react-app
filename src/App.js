24325a21189c8ec91c2f76bca8fa0c33b05e51d4
Naphtali Morage <naphtalimorage56@gmail.com>
2024-05-28 23:05:19 +0300

    import well Home component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Home from './Home';
+import Home from "./Home"
 import RegistrationForm from "./RegistrationForm";
 
 
