01cadc8a0626b346f25f594b55ff6dd24d5a7033
Naphtali Morage <naphtalimorage56@gmail.com>
2024-05-30 21:36:43 +0300

    add fonts  and remove validation function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-import Home from "./Home"
-import RegistrationForm from "./RegistrationForm";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from "./Components/Home"
+import RegistrationForm from "./Components/RegistrationForm";
 
 
 const App = () => {
